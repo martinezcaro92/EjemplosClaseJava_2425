68a9d92e3990bb8fecf96def8cdb38baee64b1b3
martinezcaro92 <martinezcaro92@gmail.com>
2025-10-11 20:48:03 +0200

    Definir ficheros básicos para clase lunes

---
+++ src/main/java/com/programacion/ejemplosclasejava_2526/Basicos/Bucles/EjWhile.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.programacion.ejemplosclasejava_2526.Basicos.Bucles;
+
+/**
+ *
+ * @author user
+ */
+public class EjWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /* ENUNCIADO: Simula el funcionamiento de un cajero automático (números positivos y negativos)
+            que se ejecute de mientras que el valor introducido sea diferente a 0 que tras introducir el valor, 
+            se diga el total que hay en la cuenta bancaria
+        */
+        
+        /* ENUNCIADO: Se define e inicializa una contraseña numérica, se preguntará una contraseña numérica
+            por teclado y se comprobará si es correcta. Se repitirá esta operación mientras no coincida la
+            contraseña.
+        */
+    }
+    
+}
---
+++ src/main/java/com/programacion/ejemplosclasejava_2526/Basicos/EstructurasControl/EjSwitchCase.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.programacion.ejemplosclasejava_2526.Basicos.EstructurasControl;
+
+/**
+ *
+ * @author user
+ */
+public class EjSwitchCase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /* ENUNCIADO: Diseña un programa que muestre un menú con operaciones: 
+            ingresar dinero, retirar dinero, consultar saldo, salir.
+        */
+        
+        /* ENUNCIADO: Solicita el tipo de producto ("alimentacion", "general", 
+            "reducido") y calcula el precio final aplicando el IVA correspondiente (4%, 10%, 21%).
+        */
+        
+        /* ENUNCIADO: Pide una nota entera (0–10) y clasifica el resultado:
+            0–4 → Suspenso
+            5 → Aprobado
+            6 → Bien
+            7–8 → Notable
+            9–10 → Sobresaliente
+        */
+
+    }
+    
+}
