22338ab1c59051e43fbb6394fa599c3f23a11c3c
José Manuel Martínez Caro (SDAW1) <josemanuel.martinez20@iesramonarcas.es>
2025-10-13 10:08:42 +0200

    Añadido código a EjWhile

--- src/main/java/com/programacion/ejemplosclasejava_2526/Basicos/Bucles/EjWhile.java
+++ src/main/java/com/programacion/ejemplosclasejava_2526/Basicos/Bucles/EjWhile.java
@@ -4,6 +4,8 @@
  */
 package com.programacion.ejemplosclasejava_2526.Basicos.Bucles;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -17,9 +19,32 @@ public class EjWhile {
         // TODO code application logic here
         
         /* ENUNCIADO: Simula el funcionamiento de un cajero automático (números positivos y negativos)
-            que se ejecute de mientras que el valor introducido sea diferente a 0 que tras introducir el valor, 
-            se diga el total que hay en la cuenta bancaria
+            que se ejecute  mientras que el valor introducido sea diferente a 0, y tras introducir el valor, 
+            se diga el total que hay en la cuenta bancaria. Comprobar que el total no sea < que 0.
         */
+        
+        Scanner scan = new Scanner (System.in);
+        int cantidad = 1;
+        int total = 0;
+        
+       
+        
+        while (cantidad != 0)
+        {
+            System.out.print("Introducir cantidad: ");
+            cantidad = scan.nextInt();
+            
+            if(cantidad==0){
+                break;
+            }   
+            
+            total +=cantidad; // total = total + cantidad
+            System.out.println("La cantidad total es: " + total);     
+        }
+        
+        
+        
+        
         
         /* ENUNCIADO: Se define e inicializa una contraseña numérica, se preguntará una contraseña numérica
             por teclado y se comprobará si es correcta. Se repitirá esta operación mientras no coincida la
