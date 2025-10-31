204b10f994d0d341d4e96b0c21af41a3d2447839
José Manuel Martínez Caro (SDAW1) <josemanuel.martinez20@iesramonarcas.es>
2025-10-31 13:39:05 +0100

    Modificado fichero Ej1 de métodos con String

--- src/main/java/com/programacion/ejemplosclasejava_2526/CadenasDeCaracteres/Ej1.java
+++ src/main/java/com/programacion/ejemplosclasejava_2526/CadenasDeCaracteres/Ej1.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -11,10 +11,14 @@ package com.programacion.ejemplosclasejava_2526.CadenasDeCaracteres;
 public class Ej1 {
 
     /**
-     * @param args the command line arguments
+     * Define una cadena con el valor " usuario@dominio.com ". 
+     * Primero, elimina los espacios en blanco al inicio y al final de la cadena. 
+     * Luego, muestra por consola la longitud final de la cadena resultante.
+
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String cadena = " usuario@dominio.com ";
     }
     
 }
